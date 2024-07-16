ccceb2c955074f20ee3b915618d49c53966a9680
Faizan Alam <faizanoffice17@gmail.com>
2024-07-16 12:46:37 +0530

    admin management controller created

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function loginAdmin(Request $request){
+        $data = $request->validate([
+            'username'=>'required',
+            'password'=> 'required',
+        ]);
+        if(Auth::attempt($data)){
+            // return "Data is present";
+            return redirect()->route('home');
+        }
+        else{
+            return redirect()->route('login')->with('status','Please Enter Correct Credentails!');
+        }
+    }   
+}
