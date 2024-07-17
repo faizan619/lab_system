a5dd4a0396626828e6308834ddda43cf51286f68
Faizan Alam <alamf6023@gmail.com>
2024-07-17 14:32:02 +0530

    routing done

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,28 @@
 <?php
 
+use App\Http\Controllers\CenterController;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\PatientController;
+use App\Http\Controllers\SubController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('page.reception.patient_registration');
-// })->name('home');
+// Route::get('/', function () { return view('page.reception.patient_registration'); })->name('home');
 
-Route::get('/login',function(){
-    return view('page.loginpage');
-})->name('login');
+Route::get('/login',function(){return view('auth.loginpage');})->name('login');
 
 Route::post('/loginuser',[UserController::class,'loginAdmin'])->name('loginUser');
 
 Route::resource('',PatientController::class);
+
+Route::get('/doctor_ref',[DoctorController::class,'showdocform'])->name('doc_ref_form');
+Route::post('/doc_data',[DoctorController::class,'docformdata'])->name('doctor_data');
+
+Route::get('/center_ref',[CenterController::class,'showcenterform'])->name('center_ref_form');
+Route::post('/center_data',[CenterController::class,'centerformdata'])->name('center_data');
+
+// Route::get('/maindept',[DepartmentController::class,''])->name('maindept');
+// Route::get('/submaindept',[SubDepartmentController::class,''])->name('subdept');
+Route::resource('dept',DepartmentController::class);
+Route::resource('subdept',SubController::class);
