d129c38887fabb3b1fe2e05a2571264b988c9210
Faizan Alam <alamf6023@gmail.com>
2024-07-23 13:36:07 +0530

    created the test UI page

---
+++ resources/views/test/helpvalue.blade.php
@@ -0,0 +1,11 @@
+@extends('masterLayout')
+
+@section('title')
+    Test Rate |
+@endsection
+
+@section('body')
+    <div class="flex items-center justify-center text-2xl h-full">
+        This is Help Value Page
+    </div>
+@endsection
