d591950ed564642d69654dc5bf3f44fee001b211
Faizan Alam <alamf6023@gmail.com>
2024-07-23 14:13:04 +0530

    routes created

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\TestController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () { return view('page.reception.patient_registration'); })->name('home');
+// admin login routes here
 
 Route::get('/login',function(){return view('auth.loginpage');})->name('login');
 Route::get('/register',function(){return view('auth.register');})->name('register');
@@ -18,9 +18,13 @@ Route::get('/register',function(){return view('auth.register');})->name('registe
 Route::post('/loginuser',[UserController::class,'loginAdmin'])->name('loginUser');
 Route::post('/registeruser',[UserController::class,'registerUser'])->name('registerUser');
 
+//reception panel routes here
+
 Route::resource('',PatientController::class);
 Route::get('/patients/search', [PatientController::class, 'search'])->name('patient.search');
 // Route::get('/patients/data', [PatientController::class, 'getData'])->name('patient.data');
+
+//master panel route here
 
 Route::get('/doctor_ref',[DoctorController::class,'showdocform'])->name('doc_ref_form');
 Route::post('/doc_data',[DoctorController::class,'docformdata'])->name('doctor_data');
@@ -37,8 +41,10 @@ Route::resource('subdept',SubController::class);
 Route::get('/search/sub', [SubController::class, 'search'])->name('sub.search');
 
 Route::resource('test',TestController::class);
-// Route::get('/test/search',[TestController::class,'search'])->name('test.search');
 Route::get('/search/test', [TestController::class, 'search'])->name('test.search');
+Route::get('/test_rate',[TestController::class,'rateview'])->name('rate');
+Route::get('/help_value',[TestController::class,'helpvalueview'])->name('help_value');
+
 
 Route::resource('parameter',ParameterController::class);
 // Route::get('/search/testpara',[ParameterController::class,'testpara'])->name('testpara.search');
@@ -46,4 +52,12 @@ Route::resource('parameter',ParameterController::class);
 Route::get('/search/parameter', [ParameterController::class, 'search'])->name('search.tests');
 
 
-
+// search panel route here
+Route::get('/search1',function(){return view('searchpanel.dailycollection');})->name('search1');
+Route::get('/search2',function(){return view('searchpanel.patientcollection');})->name('search2');
+Route::get('/search3',function(){return view('searchpanel.patient');})->name('search3');
+Route::get('/search4',function(){return view('searchpanel.dailycollection');})->name('search4');
+Route::get('/search5',function(){return view('searchpanel.dailycollection');})->name('search5');
+Route::get('/search6',function(){return view('searchpanel.dailycollection');})->name('search6');
+Route::get('/search7',function(){return view('searchpanel.dailycollection');})->name('search7');
+Route::get('/search8',function(){return view('searchpanel.dailycollection');})->name('search8');
