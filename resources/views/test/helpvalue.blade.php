c06ce0f6056b266b9ba9dc8290a8f3553a4e3414
Faizan Alam <alamf6023@gmail.com>
2024-07-24 15:03:17 +0530

    completed with add value and help value

--- resources/views/test/helpvalue.blade.php
+++ resources/views/test/helpvalue.blade.php
@@ -5,7 +5,19 @@
 @endsection
 
 @section('body')
-    <div class="flex items-center justify-center text-2xl h-full">
-        This is Help Value Page
+    <div class="flex items-center justify-center">
+        <div class="w-[60%]">
+            <h1 class="text-xl text-center font-semibold">Search Parameter for Help Value</h1>
+            <hr/>
+            <div class="flex justify-center items-center my-3 gap-3">
+                <p class="text-xl">Enter Parameter : </p>
+                <input type="text" name="parameter" id="parameter" placeholder="Enter Parameter Name" class="p-1 rounded-sm w-80">
+                <button class="px-3 py-1 bg-blue-600 text-white rounded-sm">Submit</button>
+            </div>
+            <hr>
+            
+        </div>
+
+        
     </div>
 @endsection
--- resources/views/test/rate.blade.php
+++ resources/views/test/rate.blade.php
@@ -5,7 +5,42 @@
 @endsection
 
 @section('body')
-    <div class="flex items-center justify-center text-2xl h-full">
-        This is Rate Page
+    <div class="flex items-center justify-center">
+        <div class="w-[80%]">
+            <h1 class="text-xl text-center font-semibold">Search Parameter for Help Value</h1>
+            <hr />
+            <div class="flex justify-evenlyg items-center my-3 gap-5">
+                <label for="center">
+                    <p class="font-semibold text-lg">Center</p>
+                    <select name="center" id="center" class="p-1 w-44 border border-dashed">
+                        <option value="val1">val1</option>
+                        <option value="val2">val2</option>
+                        <option value="val3">val2</option>
+                    </select>
+                </label>
+                <label for="dept">
+                    <p class="font-semibold text-lg">Department</p>
+                    <select name="department" id="dept" class="p-1 w-44 border border-dashed">
+                        <option value="val1">val1</option>
+                        <option value="val2">val2</option>
+                        <option value="val3">val2</option>
+                    </select>
+                </label>
+                <label for="subdept">
+                    <p class="font-semibold text-lg">Sub Department</p>
+                    <select name="subdept" id="subdept" class="p-1 w-44 border border-dashed">
+                        <option value="val1">val1</option>
+                        <option value="val2">val2</option>
+                        <option value="val3">val2</option>
+                    </select>
+                </label>
+                <label for="test">
+                    <p class="font-semibold text-lg">Search Test</p>
+                    <input type="text" name="test" id="test" placeholder="Search By Test Name" class="p-1">
+                </label>
+
+            </div>
+            <hr>
+        </div>
     </div>
 @endsection
