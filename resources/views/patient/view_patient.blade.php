811d9258a18a9b0a8cd9f6eeab07c61e0e575bbe
Faizan Alam <alamf6023@gmail.com>
2024-08-09 15:41:42 +0530

    complete with view patient page

--- resources/views/patient/view_patient.blade.php
+++ resources/views/patient/view_patient.blade.php
@@ -4,98 +4,114 @@
     View Patient |
 @endsection
 
-@section('body')
-    <div class="flex justify-center items-center">
-        <div class="mt-5 w-[90%] ">
-            <h1 class="text-center font-semibold text-xl">Search Patient for View Record</h1>
-            <hr class="border-blue-800" />
-            @if (session('status'))
-            <div id="alert-1" class="flex items-center p-4 my-1 text-blue-800 rounded-lg bg-blue-50" role="alert">
-                <i class="fa-solid fa-circle-info fa-lg"></i>
-                <span class="sr-only">Info</span>
-                <div class="ms-3 text-sm font-medium">
-                    {{ session('status') }}
-                </div>
-                  <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg p-1.5 hover:bg-blue-200 inline-flex items-center justify-center h-8 w-8" data-dismiss-target="#alert-1" aria-label="Close">
-                    <span class="sr-only">Close</span>
-                    <i class="fa-solid fa-xmark fa-lg"></i>
-                </button>
-              </div>
-            @endif
-            <div class="flex flex-col justify-center items-center ">
-                <div class="my-5 flex justify-between gap-5 w-[60%]">
-                    <input title="Search Patient" type="text" name="query" id="search" placeholder="Search by Aadhar No or Phone No"
-                        class="p-1 border w-full rounded-md">
-                    <a href="{{ route('create') }}">
-                        {{-- <div class="px-3 w-28 py-1 rounded-md bg-blue-700 text-white">Create New</div> --}}
-                        <p title="Create New Patient" class="bg-blue-600 text-white py-2 px-3 hover:scale-105 cursor-pointer rounded-full">
-                            <i class="fa-solid fa-plus fa-lg"></i>
-                        </p>
-                    </a>
-                </div>
-                <table class="w-full">
-                    <thead class="bg-black text-white w-full">
-                        <tr>
-                            <td class="border px-5 font-semibold bg-black text-white capitalize text-center">Id</td>
-                            <td class="border px-5 font-semibold bg-black text-white capitalize text-center">Card_No</td>
-                            <td class="border px-5 font-semibold bg-black text-white capitalize text-center">Patient</td>
-                            <td class="border px-5 font-semibold bg-black text-white capitalize text-center">Register_Date
-                            </td>
-                            <td class="border px-5 font-semibold bg-black text-white capitalize text-center">Action</td>
-                        </tr>
-                    </thead>
-                    <tbody class="border" id="patientTableBody">
-                        @forelse ($patientdata as $data)
-                            <tr>
-                                <td class="border px-1 ">{{ $data->id }}</td>
-                                <td class="border px-1 ">{{ $data->user_card_no }}</td>
-                                <td class="border px-1 ">{{ $data->fname }} {{ $data->mname }} {{ $data->lname }}</td>
-                                <td class="border px-1 ">{{ $data->register_date }}</td>
-                                <td class="border px-1 flex gap-3 flex justify-center items-center">
-                                    <a href={{ route('dept.show', $data->id) }}>
-                                        <button title="View Details"
-                                            class="px-3 border text-sm py-1 hover:bg-blue-800 rounded-md bg-blue-700 text-white"><i
-                                                class="fa-regular fa-address-card"></i></button>
-                                    </a>
-                                    <button title="Don't Know"
-                                        class="px-3 border text-sm py-1 hover:bg-green-800 rounded-md bg-green-700 text-white"><i
-                                            class="fa-solid fa-inbox fa-xl"></i></button>
-                                    <button title="Receipt"
-                                        class="px-3 border text-sm py-1 hover:bg-gray-800 rounded-md bg-gray-700 text-white"><i
-                                            class="fa-solid fa-receipt fa-lg"></i></button>
-                                </td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <td colspan="5" class="text-center">No Patient Data Available</td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
-                @if ($patientdata->hasPages())
-                    <div class="mt-5 w-full">
-                        {{ $patientdata->links() }}
+@section('content')
+    <div class="container">
+        @if (session('status'))
+            <div class="alert alert-warning d-flex align-items-center justify-content-between" id="customAlert">
+                <strong>{{ session('status') }}</strong>
+                <button type="button" class="bg-danger border-danger text-lg px-2 rounded"
+                    onclick="closeAlert()">&times;</button>
+            </div>
+        @endif
+        <div>
+            <div class="pt-3">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Search Patient For View Record</h3>
                     </div>
-                @endif
+                    <div class="card-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Card No</th>
+                                    <th>Patient</th>
+                                    <th>Register Date</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($patientdata as $data)
+                                    <tr>
+                                        <td>{{ $data->id }}</td>
+                                        <td>{{ $data->user_card_no }}</td>
+                                        <td>{{ $data->fname }} {{ $data->mname }} {{ $data->lname }}</td>
+                                        <td>{{ $data->register_date }}</td>
+                                        <td>
+                                            <a href={{ route('dept.show', $data->id) }}>
+                                                <button class="btn-outline-dark px-2 border-dark" title="View Details"><i
+                                                        class="fa-regular fa-address-card"></i></button>
+                                            </a>
+                                            <button class="btn-outline-dark px-2 border-dark" title="Don't Know"><i
+                                                    class="fa-solid fa-inbox fa-xl"></i></button>
+                                            <button class="btn-outline-dark px-2 border-dark" title="Receipt"><i
+                                                    class="fa-solid fa-receipt fa-lg"></i></button>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="5">No Patient Data Available</td>
+                                    </tr>
+                                @endforelse
+
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Card No</th>
+                                    <th>Patient</th>
+                                    <th>Register Date</th>
+                                    <th>Action</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+@endsection
+
+@section('menuopenreception')
+    menu-open
+@endsection
+
+@section('headFile')
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+@endsection
+
+@section('scriptFile')
+    <!-- DataTables  & Plugins -->
+    <script src="{{ asset('adminlte/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+    <script>
+        function closeAlert() {
+            var alert = document.getElementById('customAlert');
+            alert.style.display = 'none';
+        }
+    </script>
 
     <script>
-        $(document).ready(function() {
-            $('#search').on('keyup', function() {
-                var query = $(this).val();
-                $.ajax({
-                    url: '{{ route('patient.search') }}',
-                    type: 'GET',
-                    data: {
-                        'query': query
-                    },
-                    success: function(data) {
-                        $('#patientTableBody').html(data);
-                    }
-                });
-            });
+        $(function() {
+            $("#example1").DataTable({
+                "responsive": true,
+                "lengthChange": false,
+                "autoWidth": false,
+                "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+            }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
         });
     </script>
 @endsection
